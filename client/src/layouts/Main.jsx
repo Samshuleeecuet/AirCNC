3f3c213c84c91ab3b24d6d4704c0a99c1da6ca7b
Samshuleeecuet <samshuleeeweb@gmail.com>
2023-07-06 12:32:57 +0600

    login and signup functionality done

--- client/src/layouts/Main.jsx
+++ client/src/layouts/Main.jsx
@@ -1,9 +1,14 @@
+
+import { Outlet } from "react-router-dom"
 import Navbar from "../components/Shared/Navbar/Navbar"
 
 const Main = () => {
   return (
     <div>
       <Navbar/>
+      <div className="pt-28">
+      <Outlet/>
+      </div>
     </div>
   )
 }
